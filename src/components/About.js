cad9d1febc27857807d9eb42df93f9f63cf26d09
Piotr Opyd <opydpiotrek@gmail.com>
2019-12-17 01:10:48 +0100

    About page design + add parallax scrolling

---
+++ src/components/About.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import '../css/AboutStyle.css'
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <div className="background-0">
+                    <div className="background-text">
+                        <span className="background-text-border">
+                            Parralax webside
+                         </span>
+                    </div>
+                </div>
+
+                <section className="section section-light">
+                    <h2> Section one </h2>
+                    <hr></hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusamus ad alias animi commodi consequatur dolor
+                        dolore doloremque dolores dolorum ducimus earum est exercitationem hic illo illum, ipsum libero nesciunt odio, quae rem
+                        repellat sequi tenetur veritatis voluptate. Dolore enim fugiat ipsam itaque quidem quod rerum. Ab delectus earum eligendi et itaque quis quos
+                        rem sunt vitae voluptatem? Alias amet, dolores error ipsam iure vero voluptas. Eos, facilis minus? Aut debitis illum in porro saepe voluptas
+                        voluptates! Alias autem esse excepturi facere fugit harum,
+                        inventore laboriosam laborum, non praesentium repellendus repudiandae veniam vero. Dignissimos distinctio inventore non optio similique voluptates?
+                    </p>
+                </section>
+
+
+                <div className="background-1">
+                    <div className="background-text">
+                         <span className="background-text-border">
+                            Parralax webside
+                         </span>
+                    </div>
+                </div>
+
+
+                <section className="section section-dark">
+                    <h2> Section sec </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusamus ad alias animi commodi consequatur dolor
+                        dolore doloremque dolores dolorum ducimus earum est exercitationem hic illo illum, ipsum libero nesciunt odio, quae rem
+                        repellat sequi tenetur veritatis voluptate. Dolore enim fugiat ipsam itaque quidem quod rerum. Ab delectus earum eligendi et itaque quis quos
+                        rem sunt vitae voluptatem? Alias amet, dolores error ipsam iure vero voluptas. Eos, facilis minus? Aut debitis illum in porro saepe voluptas
+                        voluptates! Alias autem esse excepturi facere fugit harum,
+                        inventore laboriosam laborum, non praesentium repellendus repudiandae veniam vero. Dignissimos distinctio inventore non optio similique voluptates?
+                    </p>
+                </section>
+
+
+
+                <div className="background-2">
+                    <div className="background-text">
+                        <span className="background-text-border">
+                             Parralax webside
+                         </span>
+                    </div>
+                </div>
+
+                <section className="section section-dark">
+                    <h2> Section th </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusamus ad alias animi commodi consequatur dolor
+                        dolore doloremque dolores dolorum ducimus earum est exercitationem hic illo illum, ipsum libero nesciunt odio, quae rem
+                        repellat sequi tenetur veritatis voluptate. Dolore enim fugiat ipsam itaque quidem quod rerum. Ab delectus earum eligendi et itaque quis quos
+                        rem sunt vitae voluptatem? Alias amet, dolores error ipsam iure vero voluptas. Eos, facilis minus? Aut debitis illum in porro saepe voluptas
+                        voluptates! Alias autem esse excepturi facere fugit harum,
+                        inventore laboriosam laborum, non praesentium repellendus repudiandae veniam vero. Dignissimos distinctio inventore non optio similique voluptates?
+                    </p>
+                </section>
+
+                <div className="background-0">
+                    <div className="background-text">
+                        <span className="background-text-border">
+                              Parralax webside
+                         </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
